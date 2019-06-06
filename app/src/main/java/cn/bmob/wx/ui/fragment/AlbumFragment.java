032577c8bfb28484394a811c3e3b13fba576e1d4
wx <421980589@qq.com>
2019-06-06 21:32:04 +0800

    choose photo

--- app/src/main/java/cn/bmob/wx/ui/fragment/AlbumFragment.java
+++ app/src/main/java/cn/bmob/wx/ui/fragment/AlbumFragment.java
@@ -1,7 +1,11 @@
 package cn.bmob.wx.ui.fragment;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +16,8 @@ import cn.bmob.imdemo.R;
 import cn.bmob.wx.base.ParentWithNaviActivity;
 import cn.bmob.wx.base.ParentWithNaviFragment;
 import cn.bmob.wx.ui.Allalbum;
+import cn.bmob.wx.ui.ChatActivity;
+import cn.bmob.wx.util.PhotoUtils;
 
 public class AlbumFragment extends ParentWithNaviFragment {
 
@@ -51,6 +57,17 @@ public class AlbumFragment extends ParentWithNaviFragment {
             public void clickRight() {
                // startActivity(Allalbum.class, null);
               //  startActivity(PhotoActivity.class, null);
+                //选择图片
+                //图片点击。打开图库选择图片
+                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    //申请 WRITE_CONTACTS 权限
+                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                            ChatActivity.REQUEST_CODE_WRITE_STORAGE);
+                } else {
+                    PhotoUtils.selectPhoto(getActivity(), PhotoUtils.INTENT_REQUEST_CODE_ALBUM);
+
+                }
 
             }
         };
