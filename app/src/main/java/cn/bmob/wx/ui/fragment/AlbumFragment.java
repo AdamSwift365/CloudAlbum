1e8338641142b027b8157c61583a66b557ea75e5
wx <421980589@qq.com>
2019-06-07 23:37:21 +0800

    "upload a photo from local album "

--- app/src/main/java/cn/bmob/wx/ui/fragment/AlbumFragment.java
+++ app/src/main/java/cn/bmob/wx/ui/fragment/AlbumFragment.java
@@ -1,28 +1,32 @@
 package cn.bmob.wx.ui.fragment;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+
+import android.provider.MediaStore;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import butterknife.ButterKnife;
 import cn.bmob.imdemo.R;
 import cn.bmob.wx.base.ParentWithNaviActivity;
 import cn.bmob.wx.base.ParentWithNaviFragment;
 import cn.bmob.wx.ui.AllAlbumActivity;
-import cn.bmob.wx.ui.ChatActivity;
-import cn.bmob.wx.util.PhotoUtils;
+
+import static android.app.Activity.RESULT_OK;
+
 
 public class AlbumFragment extends ParentWithNaviFragment {
 
 
-
+    private static int RESULT_LOAD_IMAGE = 1;
 
     LinearLayoutManager layoutManager;
 
@@ -59,20 +63,44 @@ public class AlbumFragment extends ParentWithNaviFragment {
               //  startActivity(PhotoActivity.class, null);
                 //选择图片
                 //图片点击。打开图库选择图片
-                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    //申请 WRITE_CONTACTS 权限
-                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
-                            ChatActivity.REQUEST_CODE_WRITE_STORAGE);
-                } else {
-                    PhotoUtils.selectPhoto(getActivity(), PhotoUtils.INTENT_REQUEST_CODE_ALBUM);
+//                if (ContextCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//                        != PackageManager.PERMISSION_GRANTED) {
+//                    //申请 WRITE_CONTACTS 权限
+//                    ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+//                            ChatActivity.REQUEST_CODE_WRITE_STORAGE);
+//                } else {
+//                    PhotoUtils.selectPhoto(getActivity(), PhotoUtils.INTENT_REQUEST_CODE_ALBUM);
+//
+//                }
 
-                }
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, RESULT_LOAD_IMAGE);
 
             }
         };
     }
 
+    @Override
+    public void  onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+                if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+
+//获取返回的数据，这里是android自定义的Uri地址
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+            //获取选择照片的数据视图
+         Cursor cursor = getActivity().getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+       cursor.moveToFirst();
+            //从数据视图中获取已选择图片的路径
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+          String picturePath = cursor.getString(columnIndex);
+           cursor.close();
+            //将图片显示到界面上
+           ImageView imageView = (ImageView) getActivity().findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+            }
+
+         }
 
 
     @Nullable
